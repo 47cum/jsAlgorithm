1d30c89e1ab94ce7cabb9db13b0ee3caf9fcdf80
47cum <happy47cum@gmail.com>
2022-07-24 11:09:29 +0900

    자바스크립트 코테 인풋 샘플

---
+++ input.sample.js
@@ -0,0 +1,18 @@
+const solution = (data) => {
+  console.log(data);
+};
+
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+let data = [];
+rl.on("line", function (line) {
+  data.push(line);
+  rl.close();
+}).on("close", function () {
+  solution(data);
+  process.exit();
+});
